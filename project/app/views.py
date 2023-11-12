b65b54d03438277edfe46e96ac0af9245ea2802e
shazid-hasan <amj.shazidhasan@gmail.com>
2023-11-12 12:54:01 +0600

    total commited

---
+++ project/app/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render,redirect
+from app.models import Employees
+
+def Index(request):
+    emp=Employees.objects.all()
+
+    context ={
+        'emp':emp,
+    }
+
+
+    return render(request,'index.html',context)
+
+# Create your views here.
+def Add(request):
+    if request.method =='POST':
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        address=request.POST.get('address')
+        phone=request.POST.get('phone')
+
+        emp=Employees(
+            name =name,
+            email=email,
+            address=address,
+            phone=phone,
+        )
+        emp.save()
+        return redirect('home')
+
+    return render(request,'index.html')
+
+def Edit(request):
+    emp=Employees.objects.all()
+
+    context ={
+        'emp':emp,
+    }
+    return redirect(request,'index.html',context)
+
+def Update(request,id):
+    if request.method =='POST':
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        address=request.POST.get('address')
+        phone=request.POST.get('phone')
+
+        emp=Employees(
+            id=id,
+            name =name,
+            email=email,
+            address=address,
+            phone=phone,
+        )
+        emp.save()
+        return redirect('home')
+
+    return redirect(request,'index.html')
+
+def Delete(request,id):
+    emp = Employees.objects.filter(id = id).delete()
+
+    context={
+        'emp':emp,
+    }
+    return redirect('home')
+
+
+    
